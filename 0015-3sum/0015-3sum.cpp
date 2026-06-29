6ec01c572d7371646e2cf9a8bd0b29d65da18782
Param Nainani <144118867+ParramNainani@users.noreply.github.com>
2026-06-30 01:42:38 +0530

    Time: 2508 ms (5.04%), Space: 506.4 MB (5.44%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // vector<vector<int>> trips;
+        // set<vector<int>> s;
+        // for(int i=0;i<nums.size()-2;i++){
+        //     for(int j=i+1; j<nums.size()-1;j++){
+        //         for(int k=j+1;k<nums.size();k++){
+        //             if(nums[i]+nums[j]+nums[k] ==0){
+        // //                 vector<int> temp = {nums[i],nums[j],nums[k]};
+        // //                 sort(temp.begin() , temp.end());
+        // //                 s.insert(temp);
+        //             }
+        //         }
+        //     }
+        // }
+        // for(auto x:s){
+        //     trips.push_back(x);
+        // }
+        // return trips;
+
+                vector<vector<int>> trips;
+        set<vector<int>> s;
+
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for(int i = 0; i < n - 2; i++) {
+
+            for(int j = i + 1; j < n - 1; j++) {
+
+                int target = -(nums[i] + nums[j]);
+
+                int start = j + 1;
+                int end = n - 1;
+
+                while(start <= end) {
+
+                    int mid = start + (end - start) / 2;
+
+                    if(nums[mid] == target) {
+
+                        s.insert({nums[i], nums[j], nums[mid]});
+                        break;
+                    }
+                    else if(nums[mid] < target) {
+                        start = mid + 1;
+                    }
+                    else {
+                        end = mid - 1;
+                    }
+                }
+            }
+        }
+
+        for(auto x : s)
+            trips.push_back(x);
+
+        return trips;
+    }
+    
+};
