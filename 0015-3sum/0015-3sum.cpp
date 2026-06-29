06150c0833849e1e96d5dbd9a0af8bc3cfa5cf6a
Param Nainani <144118867+ParramNainani@users.noreply.github.com>
2026-06-30 01:57:07 +0530

    Time: 1208 ms (8.31%), Space: 295.5 MB (6.01%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -19,33 +19,62 @@ public:
         // }
         // return trips;
 
+        //     vector<vector<int>> trips;
+        //     set<vector<int>> s;
+        //     sort(nums.begin(), nums.end());
+        //     int n = nums.size();
+        //     for(int i = 0; i < n - 2; i++) {
+        //         for(int j = i + 1; j < n - 1; j++) {
+        //             int target = -(nums[i] + nums[j]);
+        //             int start = j + 1;
+        //             int end = n - 1;
+        //             while(start <= end) {
+        //                 int mid = start + (end - start) / 2;
+        //                 if(nums[mid] == target) {
+        //                     s.insert({nums[i], nums[j], nums[mid]});
+        //                     break;
+        //                 }
+        //                 else if(nums[mid] < target) {
+        //                     start = mid + 1;
+        //                 }
+        //                 else {
+        //                     end = mid - 1;
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     for(auto x : s)
+        //         trips.push_back(x);
+        //     return trips;
+        // }
+        
         vector<vector<int>> trips;
         set<vector<int>> s;
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        for(int i = 0; i < n - 2; i++) {
-            for(int j = i + 1; j < n - 1; j++) {
-                int target = -(nums[i] + nums[j]);
-                int start = j + 1;
-                int end = n - 1;
-                while(start <= end) {
-                    int mid = start + (end - start) / 2;
-                    if(nums[mid] == target) {
-                        s.insert({nums[i], nums[j], nums[mid]});
-                        break;
+        sort(nums.begin() , nums.end());
+        int left = 0;
+        int right = 0;
+        for(int i= 0 ; i<nums.size();i++){
+                left = i+1;
+                right = nums.size()-1;
+                while(left < right){
+                    int sum = nums[i] + nums[left] + nums[right];
+                    if(sum==0){
+                        s.insert({nums[i],nums[left],nums[right]});
+                        left++;
+                        right--;
                     }
-                    else if(nums[mid] < target) {
-                        start = mid + 1;
+                    else if(sum > 0){
+                        right--;
                     }
-                    else {
-                        end = mid - 1;
+                    else{
+                        left++;
                     }
                 }
             }
+            for(auto x:s){
+                trips.push_back(x);
+            }
+            return trips;
         }
-        for(auto x : s)
-            trips.push_back(x);
-        return trips;
-    }
-    
+        
 };
