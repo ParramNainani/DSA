93203e6ceb6e9d77314bc87b6a89f87f6d715004
Param Nainani <144118867+ParramNainani@users.noreply.github.com>
2026-06-30 02:00:02 +0530

    Time: 43 ms (84.19%), Space: 29.2 MB (47.28%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -48,20 +48,31 @@ public:
         //     return trips;
         // }
         
+        // 
+        
         vector<vector<int>> trips;
-        set<vector<int>> s;
+
         sort(nums.begin() , nums.end());
         int left = 0;
         int right = 0;
-        for(int i= 0 ; i<nums.size();i++){
+        for(int i= 0 ; i<nums.size()-2;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
                 left = i+1;
                 right = nums.size()-1;
+
                 while(left < right){
                     int sum = nums[i] + nums[left] + nums[right];
                     if(sum==0){
-                        s.insert({nums[i],nums[left],nums[right]});
+                        trips.push_back({nums[i],nums[left],nums[right]});
                         left++;
                         right--;
+                        while(left < right && nums[left] == nums[left - 1])
+                            left++;
+
+                        while(left < right && nums[right] == nums[right + 1])
+                            right--;
                     }
                     else if(sum > 0){
                         right--;
@@ -70,9 +81,6 @@ public:
                         left++;
                     }
                 }
-            }
-            for(auto x:s){
-                trips.push_back(x);
             }
             return trips;
         }
