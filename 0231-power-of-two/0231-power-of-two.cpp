2f8bb9b11dacd3bac8efadb96290184fed7f64d9
Param Nainani <144118867+ParramNainani@users.noreply.github.com>
2026-07-07 11:17:48 +0530

    Time: 0 ms (100%), Space: 7.7 MB (81.94%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n <= 0){
-            return false;
+        // if (n <= 0){
+        //     return false;
+        // }
+        // while(n%2==0){
+        //     n = n/2;
+        // }
+        // return n == 1;
+
+        //without loop 
+        if(n==1){
+            return true;
         }
-        while(n%2==0){
-            n = n/2;
+        if(n==2){
+            return true;
         }
-        return n == 1;
+        if(n>2){
+            if(n > 0 && (n &(n-1)) == 0){
+                return true;
+            }
+        }
+        return false;
     }
 };
