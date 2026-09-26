be4aba6442fde73142efc36926542bfd290ec56f
Parram Nainani <144118867+ParramNainani@users.noreply.github.com>
2026-09-26 22:35:59 +0530

    Time: 0 ms (100%), Space: 10.1 MB (70.07%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int start = 0;
+        int end = 0;
+        string ans = "";
+        int i = s.size() -1 ;
+        while(i >= 0){
+            while(i >= 0 && s[i] == ' '){
+                i--;
+            }
+            if(i < 0) break;
+            end = i;
+            while(i >= 0 && s[i] != ' '){
+                i--;
+            }
+            start = i+1;
+            if(!ans.empty()){
+                ans+=' ';
+            }
+            ans += s.substr(start , end - start +1);
+        }
+        return ans;
+    }
+};
